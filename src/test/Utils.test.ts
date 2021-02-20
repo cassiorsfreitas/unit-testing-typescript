f46fca68347b3353dd315e06015e785c819c2837
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2021-02-20 16:01:22 +0000

    added jest coconfig and working first test

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,10 @@
+import { Utils } from '../app/Utils'
+
+
+describe('Utils test suite', ()=>{
+    test('First test', ()=> {
+        const result = Utils.toUpperCase('abc');
+        expect(result).toBe('ABC');
+        
+    })
+});
