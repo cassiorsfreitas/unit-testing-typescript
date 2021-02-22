a6fe122f9616d4af8de8e499db24d8e9cc57a716
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2021-02-22 19:41:01 +0000

    added coverage and updated jest.config.js

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,7 +1,9 @@
-
-
 export class Utils {
-
+    
+    public static sum(a:number = 0, b:number = 0) {
+        return a + b;
+    }
+    
     public static toUpperCase(arg: string) {
         return arg.toUpperCase();
     }
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -2,6 +2,12 @@ import { Utils } from '../app/Utils'
 
 
 describe('Utils test suite', ()=>{
+
+    test('sum numbers', ()=> {
+        const result = Utils.sum(2, 3);
+        expect(result).toBe(5);
+    })
+
     test('First test', ()=> {
         const result = Utils.toUpperCase('abc');
         expect(result).toBe('ABC');
